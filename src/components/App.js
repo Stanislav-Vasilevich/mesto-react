dbeac9abc9bbe8000808bd3f5c8ee7539428e3dc
Stanislav Vasilevich <stanislavasilevich@yandex.ru>
2021-03-11 16:03:37 +0300

    refactor: add opened popups

--- src/components/App.js
+++ src/components/App.js
@@ -5,39 +5,152 @@ import PopupWithForm from './PopupWithForm.js';
 import Footer from './Footer.js';
 import './../index.css';
 
-function App(props) {
-  const[isEditProfilePopupOpen, setIsEditProfilePopupOpen] = React.useState(false);
-  const[isAddPlacePopupOpen, setIsAddPlacePopupOpen] = React.useState(false);
-  const[isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = React.useState(false);
-  const[isOpen, setIsOpen] = React.useState(false);
+function App() {
+  const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = React.useState(
+    false
+  );
+  const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = React.useState(false);
+  const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = React.useState(
+    false
+  );
 
   function handleEditAvatarClick() {
     setIsEditProfilePopupOpen(!isEditProfilePopupOpen);
-    setIsOpen(!isOpen);
-    console.log('handleEditAvatarClick');
   }
 
   function handleEditProfileClick() {
     setIsAddPlacePopupOpen(!isAddPlacePopupOpen);
-    setIsOpen(!isOpen);
-    console.log('handleEditProfileClick');
   }
 
   function handleAddPlaceClick() {
     setIsEditAvatarPopupOpen(!isEditAvatarPopupOpen);
-    setIsOpen(!isOpen);
-    console.log('handleAddPlaceClick');
   }
 
   return (
     <>
       <Header />
-      <Main 
+      <Main
         onEditAvatar={handleEditAvatarClick}
         onEditProfile={handleEditProfileClick}
         onAddPlace={handleAddPlaceClick}
       />
       <Footer />
+
+      <PopupWithForm
+        name="edit-avatar"
+        title="Обновить аватар"
+        isOpen={isEditProfilePopupOpen}
+      >
+        <label className="form__label">
+          <input
+            type="url"
+            name="form-avatar"
+            id="description-input-avatar"
+            className="form__input form__input_url-avatar"
+            placeholder="Ссылка на картинку"
+            required
+          />
+          <span
+            id="description-input-avatar-error"
+            className="form__input-error form__input-error_avatar"
+          >
+            Необходимо заполнить данное поле
+          </span>
+        </label>
+        <button type="submit" className="form__submit" disabled>
+          Сохранить
+        </button>
+      </PopupWithForm>
+
+      <PopupWithForm
+        name="edit-profile"
+        title="Редактировать профиль"
+        isOpen={isAddPlacePopupOpen}
+      >
+        <label className="form__label">
+          <input
+            type="text"
+            name="form-title"
+            id="title-input"
+            className="form__input form__input_name"
+            placeholder="Имя"
+            required
+          />
+          <span
+            id="title-input-error"
+            className="form__input-error form__input-error_name"
+          >
+            Необходимо заполнить данное поле
+          </span>
+        </label>
+        <label className="form__label">
+          <input
+            type="text"
+            name="form-subtitle"
+            id="subtitle-input"
+            className="form__input form__input_job"
+            placeholder="О себе"
+            required
+          />
+          <span
+            id="subtitle-input-error"
+            className="form__input-error form__input-error_job"
+          >
+            Необходимо заполнить данное поле
+          </span>
+        </label>
+        <button type="submit" className="form__submit">
+          Сохранить
+        </button>
+      </PopupWithForm>
+
+      <PopupWithForm
+        name="add-cards"
+        title="Новое место"
+        isOpen={isEditAvatarPopupOpen}
+      >
+        <label className="form__label">
+          <input
+            type="text"
+            name="form-title"
+            id="name-input"
+            className="form__input form__input_place"
+            placeholder="Название"
+            required
+          />
+          <span
+            id="name-input-error"
+            className="form__input-error form__input-error_name"
+          >
+            Необходимо заполнить данное поле
+          </span>
+        </label>
+        <label className="form__label">
+          <input
+            type="url"
+            name="form-subtitle"
+            id="description-input"
+            className="form__input form__input_url"
+            placeholder="Ссылка на картинку"
+            required
+          />
+          <span
+            id="description-input-error"
+            className="form__input-error form__input-error_job"
+          >
+            Необходимо заполнить данное поле
+          </span>
+        </label>
+        <button type="submit" className="form__submit " disabled>
+          Создать
+        </button>
+      </PopupWithForm>
+
+      <PopupWithForm name="delete-img" title="Вы уверены?">
+        <button type="submit" class="submit submit-delete-card">
+          Да
+        </button>
+      </PopupWithForm>
     </>
   );
 }
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -3,7 +3,7 @@ import React from 'react';
 function PopupWithForm(props) {
   return (
     <>
-      <div className={`popup popup_type_${props.name} popup_delete ${props.isOpen}`}>
+      <div className={`popup popup_type_${props.name} popup_delete ${props.isOpen ? 'popup_opened' : ''}`}>
         <div className="popup__overlay"></div>
         <div className={`popup__container popup__container_form popup__container_${props.name}`}> 
           <button type="button" className="popup__close-icon"></button>
